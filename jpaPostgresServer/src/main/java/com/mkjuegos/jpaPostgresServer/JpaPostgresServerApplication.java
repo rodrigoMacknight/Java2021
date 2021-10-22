d4e237209a6f777ec78219241d7fdbb001c4b1bf
rodrigo macknight <rodrigo.macknight@gmail.com>
2021-10-22 09:01:17 -0300

    feature/preworkout

---
+++ jpaPostgresServer/src/main/java/com/mkjuegos/jpaPostgresServer/JpaPostgresServerApplication.java
@@ -0,0 +1,13 @@
+package com.mkjuegos.jpaPostgresServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPostgresServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPostgresServerApplication.class, args);
+	}
+
+}
---
+++ jpaPostgresServer/src/test/java/com/mkjuegos/jpaPostgresServer/JpaPostgresServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkjuegos.jpaPostgresServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPostgresServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
